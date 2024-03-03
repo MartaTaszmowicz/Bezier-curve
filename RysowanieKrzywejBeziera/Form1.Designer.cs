a99fb0a761c8196e6ef9d08a33109bf3e3d09493
Marta Taszmowicz Rzepka <marta.taszmowicz@gmail.com>
2024-03-03 01:11:12 +0100

    Add project files.

---
+++ RysowanieKrzywejBeziera/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace RysowanieKrzywejBeziera
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            P1X = new TextBox();
+            P1Y = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            P2Y = new TextBox();
+            P2X = new TextBox();
+            label3 = new Label();
+            C2Y = new TextBox();
+            C2X = new TextBox();
+            label4 = new Label();
+            C1Y = new TextBox();
+            C1X = new TextBox();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            bufforSizeCtrl = new TextBox();
+            groupBox3 = new GroupBox();
+            checkBoxMoveControlPoint = new CheckBox();
+            label5 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(108, 260);
+            button1.Margin = new Padding(2);
+            button1.Name = "button1";
+            button1.Size = new Size(130, 31);
+            button1.TabIndex = 0;
+            button1.Text = "Przelicz";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // P1X
+            // 
+            P1X.Location = new Point(42, 18);
+            P1X.Margin = new Padding(2);
+            P1X.Name = "P1X";
+            P1X.Size = new Size(52, 23);
+            P1X.TabIndex = 1;
+            // 
+            // P1Y
+            // 
+            P1Y.Location = new Point(42, 40);
+            P1Y.Margin = new Padding(2);
+            P1Y.Name = "P1Y";
+            P1Y.Size = new Size(52, 23);
+            P1Y.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(8, 30);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(20, 15);
+            label1.TabIndex = 3;
+            label1.Text = "P1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(15, 32);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(20, 15);
+            label2.TabIndex = 6;
+            label2.Text = "P2";
+            // 
+            // P2Y
+            // 
+            P2Y.Location = new Point(48, 42);
+            P2Y.Margin = new Padding(2);
+            P2Y.Name = "P2Y";
+            P2Y.Size = new Size(52, 23);
+            P2Y.TabIndex = 5;
+            // 
+            // P2X
+            // 
+            P2X.Location = new Point(48, 20);
+            P2X.Margin = new Padding(2);
+            P2X.Name = "P2X";
+            P2X.Size = new Size(52, 23);
+            P2X.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(125, 32);
+            label3.Margin = new Padding(2, 0, 2, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(21, 15);
+            label3.TabIndex = 12;
+            label3.Text = "C2";
+            // 
+            // C2Y
+            // 
+            C2Y.Location = new Point(158, 42);
+            C2Y.Margin = new Padding(2);
+            C2Y.Name = "C2Y";
+            C2Y.Size = new Size(52, 23);
+            C2Y.TabIndex = 11;
+            // 
+            // C2X
+            // 
+            C2X.Location = new Point(158, 20);
+            C2X.Margin = new Padding(2);
+            C2X.Name = "C2X";
+            C2X.Size = new Size(52, 23);
+            C2X.TabIndex = 10;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(118, 30);
+            label4.Margin = new Padding(2, 0, 2, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(21, 15);
+            label4.TabIndex = 9;
+            label4.Text = "C1";
+            // 
+            // C1Y
+            // 
+            C1Y.Location = new Point(152, 40);
+            C1Y.Margin = new Padding(2);
+            C1Y.Name = "C1Y";
+            C1Y.Size = new Size(52, 23);
+            C1Y.TabIndex = 8;
+            // 
+            // C1X
+            // 
+            C1X.Location = new Point(152, 18);
+            C1X.Margin = new Padding(2);
+            C1X.Name = "C1X";
+            C1X.Size = new Size(52, 23);
+            C1X.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(P1X);
+            groupBox1.Controls.Add(P1Y);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(C1X);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(C1Y);
+            groupBox1.Location = new Point(15, 22);
+            groupBox1.Margin = new Padding(2);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new Padding(2);
+            groupBox1.Size = new Size(223, 73);
+            groupBox1.TabIndex = 13;
+            groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(label2);
+            groupBox2.Controls.Add(P2X);
+            groupBox2.Controls.Add(label3);
+            groupBox2.Controls.Add(P2Y);
+            groupBox2.Controls.Add(C2Y);
+            groupBox2.Controls.Add(C2X);
+            groupBox2.Location = new Point(14, 98);
+            groupBox2.Margin = new Padding(2);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Padding = new Padding(2);
+            groupBox2.Size = new Size(224, 73);
+            groupBox2.TabIndex = 14;
+            groupBox2.TabStop = false;
+            // 
+            // bufforSizeCtrl
+            // 
+            bufforSizeCtrl.Location = new Point(108, 185);
+            bufforSizeCtrl.Margin = new Padding(2);
+            bufforSizeCtrl.Name = "bufforSizeCtrl";
+            bufforSizeCtrl.Size = new Size(106, 23);
+            bufforSizeCtrl.TabIndex = 15;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            groupBox3.Controls.Add(checkBoxMoveControlPoint);
+            groupBox3.Controls.Add(label5);
+            groupBox3.Controls.Add(groupBox2);
+            groupBox3.Controls.Add(bufforSizeCtrl);
+            groupBox3.Controls.Add(groupBox1);
+            groupBox3.Controls.Add(button1);
+            groupBox3.Location = new Point(12, 435);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(251, 307);
+            groupBox3.TabIndex = 16;
+            groupBox3.TabStop = false;
+            // 
+            // checkBoxMoveControlPoint
+            // 
+            checkBoxMoveControlPoint.AutoSize = true;
+            checkBoxMoveControlPoint.Location = new Point(39, 226);
+            checkBoxMoveControlPoint.Name = "checkBoxMoveControlPoint";
+            checkBoxMoveControlPoint.Size = new Size(121, 19);
+            checkBoxMoveControlPoint.TabIndex = 17;
+            checkBoxMoveControlPoint.Text = "with control point";
+            checkBoxMoveControlPoint.UseVisualStyleBackColor = true;
+            checkBoxMoveControlPoint.CheckedChanged += checkBoxMoveControlPoint_CheckedChanged;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(34, 188);
+            label5.Name = "label5";
+            label5.Size = new Size(65, 15);
+            label5.TabIndex = 16;
+            label5.Text = "Buffor size:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(820, 754);
+            Controls.Add(groupBox3);
+            DoubleBuffered = true;
+            Margin = new Padding(2);
+            Name = "Form1";
+            Text = "Krzywa Beziera - POC";
+            Resize += Form1_Resize;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox P1X;
+        private TextBox P1Y;
+        private Label label1;
+        private Label label2;
+        private TextBox P2Y;
+        private TextBox P2X;
+        private Label label3;
+        private TextBox C2Y;
+        private TextBox C2X;
+        private Label label4;
+        private TextBox C1Y;
+        private TextBox C1X;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private TextBox bufforSizeCtrl;
+        private GroupBox groupBox3;
+        private Label label5;
+        private CheckBox checkBoxMoveControlPoint;
+    }
+}
---
+++ RysowanieKrzywejBeziera/Form1.cs
@@ -0,0 +1,262 @@
+﻿/*
+ * Coding: Marta Taszmowicz - 2024
+ * 
+ * WinForms POC application for drawing Bezier curve
+ * 
+ */
+
+
+using System;
+
+namespace RysowanieKrzywejBeziera
+{
+    public partial class Form1 : Form
+    {
+        // Deklaracja zmiennych
+        public Point P1 = new Point(50, 50);
+        public Point C1 = new Point(250, 100);
+        public Point P2 = new Point(600, 500);
+        public Point C2 = new Point(380, 700);
+        public int bufferSize = 25;
+        public int pointSize = 6;
+        public Bitmap buffer;
+        private bool isDraggingP1 = false;
+        private bool isDraggingP2 = false;
+        private bool isDraggingC1 = false;
+        private bool isDraggingC2 = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            P1X.Text = P1.X.ToString();
+            P1Y.Text = P1.Y.ToString();
+            C1X.Text = C1.X.ToString();
+            C1Y.Text = C1.Y.ToString();
+            P2X.Text = P2.X.ToString();
+            P2Y.Text = P2.Y.ToString();
+            C2X.Text = C2.X.ToString();
+            C2Y.Text = C2.Y.ToString();
+            bufforSizeCtrl.Text = bufferSize.ToString();
+
+            // Create a new bitmap with the same size as the form
+            buffer = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
+            RedrawCurve();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RedrawCurve();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            if (buffer != null)
+            {
+                this.SuspendLayout();
+                e.Graphics.DrawImage(buffer, 0, 0);
+                this.ResumeLayout();
+            }
+        }
+
+        void RedrawCurve()
+        {
+            using (Graphics g = Graphics.FromImage(buffer))
+            {
+                bufferSize = int.Parse(bufforSizeCtrl.Text);
+                P1.X = int.Parse(P1X.Text);
+                P1.Y = int.Parse(P1Y.Text);
+                C1.X = int.Parse(C1X.Text);
+                C1.Y = int.Parse(C1Y.Text);
+                P2.X = int.Parse(P2X.Text);
+                P2.Y = int.Parse(P2Y.Text);
+                C2.X = int.Parse(C2X.Text);
+                C2.Y = int.Parse(C2Y.Text);
+
+
+                Brush bBrush = (Brush)Brushes.Black;
+                Pen bPen = new Pen(Color.Black, 1);
+                Brush rBrush = (Brush)Brushes.Red;
+                Pen rPen = new Pen(Color.Red, 1);
+                Pen pPen = new Pen(Color.LightGray, 3);
+
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                g.Clear(Color.White);
+
+                g.DrawBezier(pPen, P1, C1, C2, P2);
+
+                Point[] points = GetBezierCurvePoints(P1, C1, P2, C2, bufferSize);
+                foreach (Point point in points)
+                {
+                    g.FillRectangle(rBrush, point.X - 1, point.Y - 1, 3, 3);
+                }
+
+                g.DrawLine(rPen, P1, C1);
+                g.DrawLine(rPen, P2, C2);
+                g.FillRectangle(bBrush, P1.X - (pointSize / 2), P1.Y - (pointSize / 2), pointSize, pointSize);
+                g.FillRectangle(rBrush, C1.X - (pointSize / 2), C1.Y - (pointSize / 2), pointSize, pointSize);
+                g.FillRectangle(bBrush, P2.X - (pointSize / 2), P2.Y - (pointSize / 2), pointSize, pointSize);
+                g.FillRectangle(rBrush, C2.X - (pointSize / 2), C2.Y - (pointSize / 2), pointSize, pointSize);
+
+                g.DrawString("P1", new Font("Arial", 10), Brushes.Black, P1.X, P1.Y);
+                g.DrawString("P2", new Font("Arial", 10), Brushes.Black, P2.X, P2.Y);
+
+                string strTemp = "Długość krzywej: " + GetBezierLength(P1, C1, P2, C2, 100).ToString();
+                g.DrawString(strTemp, new Font("Arial", 10), Brushes.Black, P2.X, P2.Y + 20);
+
+                Invalidate();
+            }
+        }
+
+        public static Point[] GetBezierCurvePoints(Point P1, Point C1, Point P2, Point C2, int bufferSize)
+        {
+            Point[] points = new Point[bufferSize];
+
+            for (int i = 0; i < bufferSize; i++)
+            {
+                double t = i / (double)(bufferSize - 1);
+
+                // Calculate coordinates of the point on the curve
+                double x = Math.Pow(1 - t, 3) * P1.X + 3 * Math.Pow(1 - t, 2) * t * C1.X + 3 * (1 - t) * Math.Pow(t, 2) * C2.X + Math.Pow(t, 3) * P2.X;
+                double y = Math.Pow(1 - t, 3) * P1.Y + 3 * Math.Pow(1 - t, 2) * t * C1.Y + 3 * (1 - t) * Math.Pow(t, 2) * C2.Y + Math.Pow(t, 3) * P2.Y;
+
+                points[i] = new Point((int)x, (int)y);
+            }
+
+            return points;
+        }
+
+        public double GetBezierLength(Point P1, Point C1, Point C2, Point P2, int bufferSize)
+        {
+            //int bufferSize = 1000; // Increase this for a more accurate result
+            double length = 0.0;
+            Point previousPoint = P1;
+
+            for (int i = 1; i <= bufferSize; i++)
+            {
+                double t = i / (double)bufferSize;
+
+                double x = Math.Pow(1 - t, 3) * P1.X + 3 * Math.Pow(1 - t, 2) * t * C1.X + 3 * (1 - t) * Math.Pow(t, 2) * C2.X + Math.Pow(t, 3) * P2.X;
+                double y = Math.Pow(1 - t, 3) * P1.Y + 3 * Math.Pow(1 - t, 2) * t * C1.Y + 3 * (1 - t) * Math.Pow(t, 2) * C2.Y + Math.Pow(t, 3) * P2.Y;
+
+                Point currentPoint = new Point((int)x, (int)y);
+
+                length += Math.Sqrt(Math.Pow(currentPoint.X - previousPoint.X, 2) + Math.Pow(currentPoint.Y - previousPoint.Y, 2));
+
+                previousPoint = currentPoint;
+            }
+
+            return length;
+        }
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+
+            // Start dragging if the mouse is within 5 pixels of any point
+            if (Math.Abs(e.X - P1.X) <= 5 && Math.Abs(e.Y - P1.Y) <= 5)
+            {
+                isDraggingP1 = true;
+            }
+            else if (Math.Abs(e.X - P2.X) <= 5 && Math.Abs(e.Y - P2.Y) <= 5)
+            {
+                isDraggingP2 = true;
+            }
+            else if (Math.Abs(e.X - C1.X) <= 5 && Math.Abs(e.Y - C1.Y) <= 5)
+            {
+                isDraggingC1 = true;
+            }
+            else if (Math.Abs(e.X - C2.X) <= 5 && Math.Abs(e.Y - C2.Y) <= 5)
+            {
+                isDraggingC2 = true;
+            }
+        }
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+
+            if (Math.Abs(e.X - P1.X) <= 5 && Math.Abs(e.Y - P1.Y) <= 5 ||
+                Math.Abs(e.X - P2.X) <= 5 && Math.Abs(e.Y - P2.Y) <= 5 ||
+                Math.Abs(e.X - C1.X) <= 5 && Math.Abs(e.Y - C1.Y) <= 5 ||
+                Math.Abs(e.X - C2.X) <= 5 && Math.Abs(e.Y - C2.Y) <= 5)
+            {
+                this.Cursor = Cursors.SizeAll;
+            }
+            else
+            {
+                this.Cursor = Cursors.Default;
+            }
+            
+            if (isDraggingP1)
+            {
+                int offsetX = C1.X - P1.X;
+                int offsetY = C1.Y - P1.Y;
+
+                P1 = new Point(e.X, e.Y);
+                if (checkBoxMoveControlPoint.Checked)
+                {
+                    C1 = new Point(P1.X + offsetX, P1.Y + offsetY);
+                }
+                P1X.Text = P1.X.ToString();
+                P1Y.Text = P1.Y.ToString();
+                C1X.Text = C1.X.ToString();
+                C1Y.Text = C1.Y.ToString();
+                RedrawCurve();
+            }
+            else if (isDraggingP2)
+            {
+                int offsetX = C2.X - P2.X;
+                int offsetY = C2.Y - P2.Y;
+
+                P2 = new Point(e.X, e.Y);
+
+                if (checkBoxMoveControlPoint.Checked)
+                {
+                    C2 = new Point(P2.X + offsetX, P2.Y + offsetY);
+                }
+
+                P2X.Text = P2.X.ToString();
+                P2Y.Text = P2.Y.ToString();
+                C2X.Text = C2.X.ToString();
+                C2Y.Text = C2.Y.ToString();
+                RedrawCurve();
+            }
+            else if (isDraggingC1)
+            {
+                C1 = new Point(e.X, e.Y);
+                C1X.Text = C1.X.ToString();
+                C1Y.Text = C1.Y.ToString();
+                RedrawCurve();
+            }
+            else if (isDraggingC2)
+            {
+                C2 = new Point(e.X, e.Y);
+                C2X.Text = C2.X.ToString();
+                C2Y.Text = C2.Y.ToString();
+                RedrawCurve();
+            }
+
+
+        }
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+
+            isDraggingP1 = false;
+            isDraggingP2 = false;
+            isDraggingC1 = false;
+            isDraggingC2 = false;
+        }
+
+        private void checkBoxMoveControlPoint_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            buffer = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
+            RedrawCurve();
+        }
+    }
+}
